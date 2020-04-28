5200f98bd7aa1059ada868272e92a57b0c8bbcba
unknown <hbopereira>
2020-04-28 16:13:47 -0300

    a

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Routes from './routes';
 function App() {
   return ( 
     <div className="App">
-      <link href="https://www.favicon-generator.org" />
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"crossorigin="anonymous"/>
        <Routes/>
     </div>
